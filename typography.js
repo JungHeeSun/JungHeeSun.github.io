9f3636f232f773714b422bd8f4e2f26eb3be3845
HeesunJung <ioiav@naver.com>
2018-12-01 22:04:39 -0500

    Add files via upload

---
+++ typography.js
@@ -0,0 +1,71 @@
+//Sep 17
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 1000) {
+        $('.schedule').addClass('visible');
+    }
+    else {
+        $('.schedule').removeClass('visible');
+    }
+
+  });
+
+//Oct 1
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 2000) {
+        $('.schedule2').addClass('visible');
+    }
+    else {
+        $('.schedule2').removeClass('visible');
+    }
+
+  });
+
+//Oct 22
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 3000) {
+        $('.schedule3').addClass('visible');
+    }
+    else {
+        $('.schedule3').removeClass('visible');
+    }
+
+  });
+
+//Nov 5
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 4000) {
+        $('.schedule4').addClass('visible');
+    }
+    else {
+        $('.schedule4').removeClass('visible');
+    }
+
+  });
+
+//Nov 19
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 5000) {
+        $('.schedule5').addClass('visible');
+    }
+    else {
+        $('.schedule5').removeClass('visible');
+    }
+
+  });
+
+//Dec 3
+$(window).scroll(function() {
+        
+    if($(this).scrollTop() >= 6000) {
+        $('.schedule6').addClass('visible');
+    }
+    else {
+        $('.schedule6').removeClass('visible');
+    }
+
+  });
